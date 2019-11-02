25e0108f06e5e0fa62f41f5d4b6f35238288bdd9
Venkata Ravila <venkataravila@wireless-10-147-30-161.public.utexas.edu>
2019-11-02 15:40:26 -0500

    Input text box changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <input id={'name'} placeholder={'Enter your name'} />
+        <input className={'text-input'} id={'name'} placeholder={'Enter your name'} />
         <a
           className="App-link"
           href="https://reactjs.org"
