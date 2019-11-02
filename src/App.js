5787614a4c681c7bc51b6dd5b6ca4d9f7f25141e
Arjun Ramesh <arjunramesh@utexas.edu>
2019-11-02 16:32:30 -0500

    Added Firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
+import firebase from 'firebase';
 import logo from './logo.svg';
-import LandingPage from "./components/LandingPage";
 import './App.css';
+import LandingPage from "./components/LandingPage";
 
-class App extends Component {
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {firebase_root: null}
+    }
+
+    componentDidMount() {
+        const firebaseConfig = {
+            apiKey: "AIzaSyBsUEvz7Kygc2E-F_3gviK9w-pbuq_Ky58",
+            authDomain: "nudge-5bf87.firebaseapp.com",
+            databaseURL: "https://nudge-5bf87.firebaseio.com",
+            projectId: "nudge-5bf87",
+            storageBucket: "nudge-5bf87.appspot.com",
+            messagingSenderId: "373645012858",
+            appId: "1:373645012858:web:83c2f54744c54cec1db0c3",
+            measurementId: "G-67S1QSXRT8"
+        };
+
+        try {
+            firebase.initializeApp(firebaseConfig);
+            console.log("Firebase Initialized");
+        } catch (err) {
+            console.log(err);
+        }
+        // create a reference to a classroom
+        const firebase_root = firebase.firestore();
+        //firebase_root.collection("users").add({name: "Jimmy", id: {emailid: 2, utid: 3}});
+        this.setState({firebase_root});
+    }
+
     render() {
         return (
-            <LandingPage/>
-        )
+            <LandingPage />
+        );
     }
 }
-
-export default App;
